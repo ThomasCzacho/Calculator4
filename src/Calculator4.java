9ba916f97e0277cda490495e9febb9998d17753f
ThomasCzacho <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-10-20 17:02:32 -0400

    Second COmmit

--- src/Calculator4.java
+++ src/Calculator4.java
@@ -2,12 +2,15 @@ public class Calculator4 {
     public double tax = .05;
     public double tip = .15;
     public double added = 30/6+15/6;
+    public double subtotal =0;
 
     //Include the cost of Alex's and Forgetful's meals in your calculations
     //Return the total after calculating
     public void findTotal(double price, String name){
         double total = (price*(1+tax+tip))+added;
+        subtotal=subtotal+total;
         System.out.println(name +": $" +total);
+        System.out.println("Current Subtotal: $"+subtotal);
 
     }
 }
